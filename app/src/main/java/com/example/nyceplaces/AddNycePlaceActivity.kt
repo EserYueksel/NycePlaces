b26813f5e2b7d2b863810e35f804a5b50f22a576
EserYueksel <eseryuksel@live.de>
2023-02-16 13:51:58 +0100

    - TODO set on AddNycePlaceActivity.kt

--- app/src/main/java/com/example/nyceplaces/AddNycePlaceActivity.kt
+++ app/src/main/java/com/example/nyceplaces/AddNycePlaceActivity.kt
@@ -3,9 +3,10 @@ package com.example.nyceplaces
 import android.app.DatePickerDialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import java.util.*
 
-class AddNycePlaceActivity : AppCompatActivity() {
+class AddNycePlaceActivity : AppCompatActivity(), View.OnClickListener {
 
     private var cal = Calendar.getInstance()
     private lateinit var dateSetListener: DatePickerDialog.OnDateSetListener
@@ -20,5 +21,26 @@ class AddNycePlaceActivity : AppCompatActivity() {
         // TODO toolbar_add_place.setNavigationOnClickListener {
         // TODO onBackPressed()
         // TODO }
+    }
+
+    // TODO dateSetListener = DatePickerDialog.OnDateSetListener {
+    // TODO view, year, month, dayOfMonth ->
+    // TODO cal.set(Calendar.YEAR, year)
+    // TODO cal.set(Calendar.MONTH, month)
+    // TODO cal.set(Calendar.DAY_OF_MONTH, dayOfMonth)
+    // TODO }
+
+    // TODO et_date.setOnClickListener(this)
+
+    override fun onClick(v: View?) {
+        when(v!!.id){
+            R.id.et_date ->{
+                DatePickerDialog(this@AddNycePlaceActivity,
+                    dateSetListener,
+                    cal.get(Calendar.YEAR),
+                    cal.get(Calendar.MONTH),
+                    cal.get(Calendar.DAY_OF_MONTH)).show()
+            }
+        }
     }
 }
