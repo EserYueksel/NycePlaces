6d7aa8a58c1674e100c2c301d5f6584afce68743
EserYueksel <eseryuksel@live.de>
2023-02-16 08:38:06 +0100

    - TODO set on AddNycePlaceActivity.kt

--- app/src/main/java/com/example/nyceplaces/AddNycePlaceActivity.kt
+++ app/src/main/java/com/example/nyceplaces/AddNycePlaceActivity.kt
@@ -1,14 +1,24 @@
 package com.example.nyceplaces
 
+import android.app.DatePickerDialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import java.util.*
 
 class AddNycePlaceActivity : AppCompatActivity() {
+
+    private var cal = Calendar.getInstance()
+    private lateinit var dateSetListener: DatePickerDialog.OnDateSetListener
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add_nyce_place)
 
         setSupportActionBar(findViewById(R.id.toolbar_add_place))
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        // TODO toolbar_add_place.setNavigationOnClickListener {
+        // TODO onBackPressed()
+        // TODO }
     }
 }
